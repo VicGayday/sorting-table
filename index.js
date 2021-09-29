af6db33aa374b876551cccf717afa8bd9b4a3dfa
Victoria <sivosha2006@mail.ru>
2021-09-29 22:07:46 +0300

    added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, "./client")))
+app.use(express.static(path.join(__dirname, "./client/public")))
 
 app.use('/api', pointsRouter)
 
